6e57a04f3b81bddf68aca2438e1efa1db14a3f63
albim72 <marcin.albiniak72@gmail.com>
2025-01-07 15:45:18 +0100

    Update main.py

--- DZIEN_1/prostaSiecNeuronowa/main.py
+++ DZIEN_1/prostaSiecNeuronowa/main.py
@@ -5,14 +5,14 @@ from simplenn import SimpleNeuralNetwork
 network = SimpleNeuralNetwork()
 print(network)
 
-train_inputs = np.array([[1,1,0],[1,1,1],[1,1,0],[1,0,0],[0,1,1],[0,1,0]])
-train_outputs = np.array([[1,0,1,1,0,1]]).T
-train_iterators = 50_000
+train_inputs = np.array([[1,1,0],[1,1,1],[1,1,0],[1,0,0],[0,1,1],[0,1,0],[0,0,0],[0,0,1]])
+train_outputs = np.array([[1,0,1,1,0,1,1,0]]).T
+train_iterators = 150_000
 
 network.train(train_inputs,train_outputs,train_iterators)
 print(f"wytrenowane wagi:\n{network.weights}")
 
-print("____________ ocena modelu __________")
+print("____________ predykcja - ocena modelu __________")
 testdata = np.array([[1,1,1],[1,0,0],[0,1,1],[0,1,0],[0,0,1],[0,0,0]])
 
 for data in testdata:
