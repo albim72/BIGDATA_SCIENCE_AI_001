0810247d111ec65692cba90e3def0d9c8e958c78
albim72 <marcin.albiniak72@gmail.com>
2025-01-10 10:32:13 +0100

    Create onemaxproblem.py

---
+++ DZIEN_4/onemaxproblem.py
@@ -0,0 +1,31 @@
+import random
+from deap import base
+from deap import creator
+from deap import tools
+
+creator.create("FitnessMax",base.Fitness,weights=(1.0,))
+creator.create("Individual",list,fitness=creator.FitnessMax)
+
+toolbox = base.Toolbox()
+
+toolbox.register("attr_bool",random.randint,0,1)
+toolbox.register("individual",tools.initRepeat,creator.Individual,toolbox.attr_bool,100)
+toolbox.register("population",tools.initRepeat,toolbox.individual)
+
+#funkcja fitness  - funkcja przystosowania - oceny
+def evalOneMax(individual):
+    return sum(individual)
+
+#rejestracja funkcji przystosowania
+toolbox.register("evaluate",evalOneMax)
+
+#rejestracja operatora krzyżowania
+toolbox.register("mate",tools.cxTwoPoint)
+
+#rejestracja operatora mutacji
+toolbox.register("mutate",tools.mutFlipBit,indpb=0.05)
+
+#rejestracja operatora selekcji
+toolbox.register("select",tools.selTournament,tournsize=3)
+
+print("_"*70)
